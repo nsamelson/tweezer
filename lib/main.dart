6c9e16599a64c2736416c774c25a9e22e68747eb
Pedropinto29 <pedrodacostapinto@gmail.com>
2022-03-23 13:06:06 +0100

    add firebase to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'Views/login_page.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
